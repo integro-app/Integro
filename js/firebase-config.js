98442108e90da0eeb7df060a44381299b02785de
integro-app <henriquemuniz.midia@gmail.com>
2026-04-29 23:09:20 -0300

    config firebase base

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// CONFIG FIREBASE ÍNTEGRO
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwqnPjj3b8DbTS-27J3p1SYIwGTX8W89g",
+  authDomain: "integro-novo.firebaseapp.com",
+  projectId: "integro-novo",
+  storageBucket: "integro-novo.firebasestorage.app",
+  messagingSenderId: "234462716664",
+  appId: "1:234462716664:web:7a745f7dac04095161d10d"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
